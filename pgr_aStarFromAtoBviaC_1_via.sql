33d5824f9619bd3734b82ab0dfba892b4c2df66b
Mohammed Zia <mohammed.zia33@gmail.com>
2016-06-27 15:21:16 +0300

    AtoBviaC and AtoBviaC_1_via

--- pgr_aStarFromAtoBviaC_1_via.sql
+++ pgr_aStarFromAtoBviaC_1_via.sql
@@ -1,12 +1,7 @@
--- THE FOLLOWING PROCEDURAL FUNCTION WILL GIVE US THE LINE CONNECTING DESTINATION POINTS --
--- The point procedural function is not necessary as starting and ending point will be shown in different colors --
--- and the intermediate in different, so there shouldn't be any confusion --
--- Enter the variadic coordinates in the order "start", "via" etc etc, "end" --
-
-create or replace function pgr_aStarFromAtoBviaC_line(IN tbl character varying, 
+create or replace function pgr_aStarFromAtoBviaC_line_1_via(IN tbl character varying,
 variadic double precision[],
-OUT seq integer, 
-OUT gid integer, 
+OUT seq integer,
+OUT gid integer,
 OUT name text,
 OUT cost double precision,
 OUT geom geometry)
@@ -38,13 +33,12 @@ begin
 		b := a+1;
 		y1 := $2[b];
 		--Use the execute statement
-		execute 'insert into matrix (id, node_id, x, y) 
+		execute 'insert into matrix (id, node_id, x, y)
 			select '||i||', id, st_x(the_geom)::double precision, st_y(the_geom)::double precision
-			from ways_vertices_pgr ORDER BY the_geom <-> ST_GeometryFromText(''Point('||x1||' '||y1||')'', 4326) limit 1;';		
+			from ways_vertices_pgr ORDER BY the_geom <-> ST_GeometryFromText(''Point('||x1||' '||y1||')'', 4326) limit 1;';
 	End Loop;
 	-- TSP nodes order calculation --
-	sql_tsp := 'select seq, id1, id2, round(cost::numeric, 5) AS cost from
-			pgr_tsp(''select id, x, y from matrix order by id'', 1, '||arrayLengthHalf||')';
+	sql_tsp := 'select id as id2 from matrix order by id';
 	-- Extracting the geom values row by row --
 	seq := 0;
 	source_var := -1;
@@ -56,7 +50,7 @@ begin
 			Else
 				execute 'select node_id from matrix where id = '||rec_tsp.id2||'' into node;
 				target_var := node.node_id;
-				sql_astar := 'SELECT gid, the_geom, name, cost, source, target, 
+				sql_astar := 'SELECT gid, the_geom, name, cost, source, target,
 						ST_Reverse(the_geom) AS flip_geom FROM ' ||
 						'pgr_astar(''SELECT gid as id, source::integer, target::integer, '
 						|| 'length::double precision AS cost, '
@@ -64,7 +58,7 @@ begin
 						|| 'x2::double precision, y2::double precision,'
 						|| 'reverse_cost::double precision FROM '
 						|| quote_ident(tbl) || ''', '
-						|| source_var || ', ' || target_var 
+						|| source_var || ', ' || target_var
 						|| ' , true, true), '
 						|| quote_ident(tbl) || ' WHERE id2 = gid ORDER BY seq';
 				For rec_astar in execute sql_astar
@@ -78,30 +72,19 @@ begin
 					End Loop;
 				source_var := target_var;
 			END IF;
-		END LOOP;	
+		END LOOP;
 	-- Drop the temporary table, otherwise the next time you will run the query it will show that the matrix table already exists --
 	drop table matrix;
 	return;
 
-	EXCEPTION
-		WHEN internal_error THEN
-			seq := seq +1 ;
+	--EXCEPTION
+		--WHEN internal_error THEN
+			--seq := seq +1 ;
 			--gid := rec_astar.gid;
 			--name := rec_astar.name;
-			cost := 9999.9999;
+			--cost := 9999.9999;
 			--geom := rec_astar.the_geom;
 
 end;
 $body$
 language plpgsql volatile STRICT;
-
--- In order to use this function --
--- Enter the variadic coordinates in the order "start", "via" etc etc, "end" --
--- SELECT geom FROM pgr_aStarFromAtoBviaC_line('ways', 28.231233, 41.324324, 29.432432, 42.423542, 30.234342, 43.234543, 28.443234, 42.454355) --
--- SELECT cost FROM pgr_aStarFromAtoBviaC_line('ways', 28.231233, 41.324324, 29.432432, 42.423542, 30.234342, 43.234543, 28.443234, 42.454355) --
--- etc etc --
--- In GeoServer, we make an SQL View like the following --
--- SELECT geom FROM pgr_aStarFromAtoBviaC_line('ways', %variadicArray%) ORDER BY seq --
--- and give the default values this 28.94603,41.00764,28.95402,41.01789,28.96145,41.00522,28.96650,41.01191 --
--- But still have to figure out how to build a request from the OpenLayers3? --
--- For time being, make different SQL Views for different possibilities (4pt, 5pt, 6pt etc)
